f45cb69a700357e8d3906ef1ef2e8b734f75cc79
unknown <a0548428751@gmail.com>
2024-07-06 22:40:36 +0300

    serverSelectionTimeoutMS

--- server/DL/connectToMongo.js
+++ server/DL/connectToMongo.js
@@ -1,14 +1,22 @@
-import mongoose,{ connect } from "mongoose";
+import mongoose from 'mongoose';
 
 export const connectToMongo = async () => {
-   try {
-      if (mongoose.connection.readyState === 1) {
-         console.log('already connected');
-         return;
-      }
-   await connect(process.env.MONGO_URL)
-   console.log('connected to mongo');
-} catch (error) {
-   console.log('error connect to mongo',error);
-}
-}
+  try {
+    if (mongoose.connection.readyState === 1) {
+      console.log('Already connected to MongoDB');
+      return;
+    }
+    
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000 
+    });
+    
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB', error);
+    throw new Error('Failed to connect to MongoDB');
+  }
+};
