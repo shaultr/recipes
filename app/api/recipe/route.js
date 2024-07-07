0821c0e8405bf37d1406d54c08cfed0450f7ddb1
unknown <a0548428751@gmail.com>
2024-07-07 16:30:40 +0300

    try

--- app/api/recipe/route.js
+++ app/api/recipe/route.js
@@ -13,8 +13,11 @@ cloudinary.v2.config({
 // GET פונקציית ה-
 export const GET = async (req) => {
   try {
+    console.log('Connecting to MongoDB...');
     await connectToMongo();
+    console.log('Fetching recipes...');
     const recipes = await readRecipesService();
+    console.log('Recipes fetched:', recipes);
     return NextResponse.json(recipes);
   } catch (error) {
     console.error('Error fetching recipes:', error);
@@ -32,14 +35,15 @@ export const POST = async (req) => {
       return NextResponse.json({ error: 'Missing required parameter - img' }, { status: 400 });
     }
 
+    console.log('Uploading image to Cloudinary...');
     const result = await cloudinary.v2.uploader.upload(imageFile, {
       folder: "instructions-images"
     });
 
-    console.log(":rocket: ~ POST ~ result:", result);
+    console.log('Image uploaded:', result);
     return NextResponse.json({ url: result.secure_url });
   } catch (err) {
-    console.error('Image upload failed:', err);
+    console.error('Error uploading image:', err);
     return NextResponse.json({ error: 'Image upload failed', details: err.message }, { status: 500 });
   }
 };
